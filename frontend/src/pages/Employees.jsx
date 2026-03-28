7da1c4151bf3be6e02857173b4d15093c2255663
Kartik-0108 <kartikdevadiga01@gmail.com>
2026-03-29 00:53:22 +0530

    ui changes

--- frontend/src/pages/Employees.jsx
+++ frontend/src/pages/Employees.jsx
@@ -1,5 +1,5 @@
 import React, { useContext, useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
+ import { useNavigate } from "react-router-dom";
 import { AuthContext } from "../context/AuthContext";
 import API from "../services/api";
 
